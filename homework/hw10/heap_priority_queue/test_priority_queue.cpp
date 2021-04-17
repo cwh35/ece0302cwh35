51ace8d3fb0a4956b569099a9ae42a9e3bd84627
Cameron <cwh35@pitt.edu>
2021-04-17 13:09:02 -0400

    modified hw10 code

--- homework/hw10/heap_priority_queue/test_priority_queue.cpp
+++ homework/hw10/heap_priority_queue/test_priority_queue.cpp
@@ -1,27 +1,38 @@
-#include <cassert>
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_COLOUR_NONE
+#include "catch.hpp"
 
-#include "dynamic_array_list.h"
 #include "heap_priority_queue.h"
 
-int main()
-{
+TEST_CASE("Test Construction", "[construction]") {
   HeapPriorityQueue<int>  pq;
-  assert(pq.isEmpty());
-  
+  REQUIRE(pq.isEmpty());
+}
+
+TEST_CASE("Test add", "[add]") {
+  HeapPriorityQueue<int>  pq;
   pq.add(25);
-  assert(!pq.isEmpty());
-  assert(pq.peek() == 25);
+
+  REQUIRE(!pq.isEmpty());
+  REQUIRE(pq.peek() == 25);
 
   pq.add(1);
-  assert(pq.peek() == 25);
+  REQUIRE(pq.peek() == 25);
 
   pq.add(78);
-  assert(pq.peek() == 78);
+  REQUIRE(pq.peek() == 78);
+}
+
+TEST_CASE("Test remove", "[remove]") {
+  HeapPriorityQueue<int>  pq;
+  pq.add(25);
+  pq.add(1);
+  pq.add(78);
 
   pq.remove();
+  REQUIRE(pq.peek() == 25);
   pq.remove();
+  REQUIRE(pq.peek() == 1);
   pq.remove();
-  assert(pq.isEmpty());
-
-  return 0;
+  REQUIRE(pq.isEmpty());
 }
